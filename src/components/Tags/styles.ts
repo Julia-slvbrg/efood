e118bda87989363649fb619d2206771a342ed1e1
Julia-slvbrg <julia.slvb@gmail.com>
2024-08-22 19:44:07 -0300

    chore: cardTag created and styled

---
+++ src/components/Tags/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { colors } from '../../styles'
+import { Props } from '.'
+
+export const TagContainer = styled.div<Props>`
+  background-color: ${colors.red};
+  color: ${colors.bege2};
+  font-size: ${(props) => `${props.fontSize}px`};
+  font-weight: 700;
+  display: inline-block;
+  padding: ${props => props.position === 'top'? '6px 4px' : '4px 6px'};
+`
