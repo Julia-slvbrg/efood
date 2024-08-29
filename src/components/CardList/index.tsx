83aba30091c91755b2b217bc2b65230a0a14cf18
Julia-slvbrg <julia.slvb@gmail.com>
2024-08-29 18:49:30 -0300

    chore: card element styled and implemented

---
+++ src/components/CardList/index.tsx
@@ -0,0 +1,25 @@
+import Restaurant from '../../models/Restaurant'
+import Card from '../Card'
+import { List } from './styles'
+
+
+export type Props = {
+  restaurantPlace: Restaurant[]
+}
+
+export const CardList = ({ restaurantPlace }: Props) => {
+  return (
+    <List>
+      {restaurantPlace.map((restaurant) => (
+        <Card
+          key={restaurant.id}
+          placeName={restaurant.placeName}
+          info={restaurant.info}
+          image={restaurant.image}
+          rate={restaurant.rate}
+          description={restaurant.description}
+        />
+      ))}
+    </List>
+  )
+}
--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,13 +1,12 @@
-import { TagContainer } from "./styles"
+import { TagContainer } from './styles'
 
 export type Props = {
   children: string
-  fontSize: string
   position?: 'top' | 'bottom'
 }
 
-export const Tag = ({ children, fontSize, position = 'top' }: Props) => {
-  return <TagContainer fontSize={fontSize} position={position}>{children}</TagContainer>
+export const Tag = ({ children, position = 'top' }: Props) => {
+  return <TagContainer position={position}>{children}</TagContainer>
 }
 
 export default Tag
